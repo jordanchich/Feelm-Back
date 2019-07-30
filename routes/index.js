03551885658da0ed8d071c1bd03c455bc77f2b3c
Youssef Boulhilat <yboulhilat@gmail.com>
2019-07-30 21:33:47 +0200

    Face 2

--- routes/index.js
+++ routes/index.js
@@ -63,7 +63,7 @@ router.post('/upload', function (req, res, next) {
               userModel.findOne({
                 _id: '5d39ad5db95aac8babb552c3'
               }, function (err, user) {
-                user.pictures.push({
+                  user.pictures.updateOne({
                   pictureName: result.original_filename,
                   pictureUrl: result.secure_url,
                   smile: jsonResponse[0].faceAttributes.smile,
